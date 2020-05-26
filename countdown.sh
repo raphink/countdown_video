e8138312f8b833af247e50f9f66177356b8eef4d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-05-26 11:04:49 +0200

    Add optional text

--- countdown.sh
+++ countdown.sh
@@ -2,20 +2,22 @@
 
 : ${fps:=1}
 : ${seconds:=60}
-: ${fontSize:=180}
+: ${fontSize:=130}
 : ${fontColor:=black}
 : ${fontFile:=/home/raphink/.local/share/fonts/FrancoisOne.ttf}
 : ${imageFile:=./meetup_2020-05_1080p.png}
 #: ${positionX:="(w-text_w)/2"}
-: ${positionX:="0.1*w"}
+: ${positionX:="0.05*w"}
 #: ${positionY:="(h-text_h)/2"}
-: ${positionY:="0.9*h-text_h"}
+: ${positionY:="0.8*h-text_h"}
+: ${textPositionY:="0.8*h-3*text_h"}
 : ${soundFile:=./Robot_Coupe_No_Vocal_-_Lost_European.mp3}
 : ${outFile:="meetup_wait_2020-05_1080p.mp4"}
+: ${text:=""}
 
 ffmpeg -loop 1 \
 	-i $imageFile \
 	-stream_loop -1 -i $soundFile \
 	-c:v libx264 -r $fps -t $seconds -pix_fmt yuv420p \
-	-vf "fps=$fps,drawtext=fontfile='$fontFile':fontcolor=$fontColor:fontsize=$fontSize:x=$positionX:y=$positionY:text='%{eif\:($seconds-mod($seconds-t,60))/60\:d\:2}\:%{eif\:mod($seconds-t,60)\:d\:2}'" \
+	-vf "fps=$fps,drawtext=fontfile='$fontFile':fontcolor=$fontColor:fontsize=$fontSize:x=$positionX:y=$textPositionY:text='$text',drawtext=fontfile='$fontFile':fontcolor=$fontColor:fontsize=$fontSize:x=$positionX:y=$positionY:text='%{eif\:($seconds-mod($seconds-t,60))/60\:d\:2}\:%{eif\:mod($seconds-t,60)\:d\:2}'" \
 	"$outFile"
